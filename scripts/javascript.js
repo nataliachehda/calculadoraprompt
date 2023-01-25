ad8d6ae83b74045ed4ae15e40d350ce50baa6435
nataliachehda <116130811+nataliachehda@users.noreply.github.com>
2023-01-25 19:36:45 -0300

    correcciones

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -21,19 +21,19 @@ if (operacion ==1) {
     alert (`tu resultado es ${resultado}`);
 }
 else if (operacion ==2) {
-    let numero1= prompt ("primer numero para sumar");
-    let numero2= prompt ("primer numero para sumar");
+    let numero1= prompt ("primer numero para restar");
+    let numero2= prompt ("primer numero para restar");
     resultado = restar (numero1, numero2)
     alert (`tu resultado es ${resultado}`);
 }
 else if (operacion ==3) {
-    let numero1= prompt ("primer numero para sumar");
-    let numero2= prompt ("primer numero para sumar");
+    let numero1= prompt ("primer numero para dividir");
+    let numero2= prompt ("primer numero para dividir");
     resultado = dividir (numero1, numero2);
 }
 else if (operacion ==4) {
-    let numero1= prompt ("primer numero para sumar");
-    let numero2= prompt ("primer numero para sumar");
+    let numero1= prompt ("primer numero para multiplicar");
+    let numero2= prompt ("primer numero para multiplicar");
     resultado = multiplicar (numero1, numero2)
     alert (`tu resultado es ${resultado}`);
 }
